476047b5ceeb84e0a221e7c5680c4414b2c25be5
bifinbabu <bifinbabu088@gmail.com>
2022-11-03 17:30:08 +0530

    Netflix originals youtube issue resolved

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,4 @@
 import {API_KEY} from './constants/constants'
 
-export const originals = `discover/tv?api_key=${API_KEY}&with_networks=213`;
+export const originals = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
 export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
